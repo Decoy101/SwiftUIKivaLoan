82664129436a4c3a0fb29901c2db0a62ff7f13c9
Decoy101 <00amangpt00@gmail.com>
2023-12-29 15:18:15 +0530

    Initial Commit

---
+++ SwiftUIKivaLoan/SwiftUIKivaLoanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKivaLoanApp.swift
+//  SwiftUIKivaLoan
+//
+//  Created by Aman Gupta on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKivaLoanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
